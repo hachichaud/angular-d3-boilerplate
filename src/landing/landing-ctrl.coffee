11b82b4138e90dc968b0a5107abce5698983ed5c
Sophie Despeisse <saucisse.jaune@gmail.com>
2017-02-14 17:59:03 +0100

    default to js

--- src/landing/landing-ctrl.coffee
+++ src/landing/landing-ctrl.coffee
@@ -1,19 +1,21 @@
-angular.module '%module%.landing'
-.controller 'LandingCtrl',
-($scope, $http) ->
-  $scope.someText = "Test"
+# To use this, comment the .js version and uncomment this one
 
-  $scope.users = []
-
-  $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN']
-
-  $http.get 'data/users.csv'
-  .then (res) ->
-    $scope.users = Papa.parse(
-      res.data
-    ,
-      header: true
-      delimiter: ','
-      dynamicTyping: true
-      skipEmptyLines: true
-    ).data
+# angular.module '%module%.landing'
+# .controller 'LandingCtrl',
+# ($scope, $http) ->
+#   $scope.someText = "Test"
+#
+#   $scope.users = []
+#
+#   $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN']
+#
+#   $http.get 'data/users.csv'
+#   .then (res) ->
+#     $scope.users = Papa.parse(
+#       res.data
+#     ,
+#       header: true
+#       delimiter: ','
+#       dynamicTyping: true
+#       skipEmptyLines: true
+#     ).data
--- src/landing/landing-ctrl.js
+++ src/landing/landing-ctrl.js
@@ -1,18 +1,18 @@
 // To use this, comment the .coffee version and uncomment this one
 
-// angular.module('%module%.landing')
-// .controller('LandingCtrl', function($scope, $http) {
-//
-//   $scope.users = [];
-//   $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
-//
-//   $http.get('data/users.csv')
-//   .then(function(res) {
-//     $scope.users = Papa.parse(res.data, {
-//       header: true,
-//       delimiter: ',',
-//       dynamicTyping: true,
-//       skipEmptyLines: true
-//     }).data;
-//   });
-// });
+angular.module('%module%.landing')
+.controller('LandingCtrl', function($scope, $http) {
+
+  $scope.users = [];
+  $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
+
+  $http.get('data/users.csv')
+  .then(function(res) {
+    $scope.users = Papa.parse(res.data, {
+      header: true,
+      delimiter: ',',
+      dynamicTyping: true,
+      skipEmptyLines: true
+    }).data;
+  });
+});
