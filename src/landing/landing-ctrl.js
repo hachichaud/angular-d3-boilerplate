eb34afb611be0bb4315a91e2aa1e3b41a3d56e58
hachichaud <saucisse.jaune@gmail.com>
2017-04-10 20:06:34 +0200

    user exercice

--- src/landing/landing-ctrl.js
+++ src/landing/landing-ctrl.js
@@ -2,17 +2,43 @@
 
 angular.module('%module%.landing')
 .controller('LandingCtrl', function($scope, $http) {
-
-  $scope.users = [];
-  $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
+  var LandingCtrl = this;
+  LandingCtrl.users = [];
+  LandingCtrl.test = "abc";
+  LandingCtrl.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
+  LandingCtrl.toggleRole = toggleRole;
 
   $http.get('data/users.csv')
   .then(function(res) {
-    $scope.users = Papa.parse(res.data, {
+    LandingCtrl.users = Papa.parse(res.data, {
       header: true,
       delimiter: ',',
       dynamicTyping: true,
       skipEmptyLines: true
     }).data;
+
+    LandingCtrl.users = LandingCtrl.users.map(function(user) {
+        user.initials = toInitial(user.firstname) + toInitial(user.lastname);
+
+        user.roleList = user.roles.split(";");
+        user.checkRole = LandingCtrl.availableRoles.map(function(item){
+           return {roleName : item, checked : user.roles.includes(item)};
+        });
+        return user;
+      })
   });
+
+  function toggleRole(role) {
+    console.log(role);
+    role.checked = !role.checked;
+  }
+
+  function toInitial(firstName){
+    var initials = "";
+    var tmpArr = firstName.split("-");
+    tmpArr.map(function(item) {
+      initials += item[0];
+    });
+    return initials;
+  }
 });
